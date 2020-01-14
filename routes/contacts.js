6e008578bee1d07363c4ab7db684aa9f3a2dbcf0
yolixtly <yolixtly_13@hotmail.com>
2020-01-13 18:40:12 -0800

    Get  Contacts Route

--- routes/contacts.js
+++ routes/contacts.js
@@ -28,8 +28,32 @@ router.get('/', auth, async (req, res) => {
 router.post('/', [
     auth,
     check('name', 'Name is required').not().isEmpty()
-], () => {
+], async (req, res) => {
+    const errors = validationResult(req);
 
+    if (!errors.isEmpty()) {
+        return res.status(400).json({
+            errors: errors.array()
+        });
+    }
+    const { name, email, phone, type } = req.body;
+
+    try {
+        const newContact = new Contact({
+            name,
+            email,
+            phone,
+            type,
+            user: req.user.id
+        });
+
+        const contact = await newContact.save();
+
+        res.json(contact);
+    } catch (error) {
+        console.error(error.message);
+        res.status(500).send('Server Error');
+    }
 });
 
 // @route POST api/contacts/:id
