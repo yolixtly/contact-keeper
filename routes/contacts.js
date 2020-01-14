9babe77d85052bbede1ce3586756ffb1adb4ff26
yolixtly <yolixtly_13@hotmail.com>
2020-01-13 18:33:39 -0800

    Basic Get User's Contacts Route

--- routes/contacts.js
+++ routes/contacts.js
@@ -1,18 +1,35 @@
 const express = require('express');
 const router = express.Router();
+const auth = require('../middleware/auth');
+const { check, validationResult } = require('express-validator');
 
+
+const User = require('../models/User');
+const Contact = require('../models/Contact');
 // @route POST api/contacts
 // @desc Get all users contacts
 // @access Private
-router.get('/', () => {
-    res.send('Get all Contacts');
+router.get('/', auth, async (req, res) => {
+    try {
+        const contacts = await Contact.find({
+            user: req.user.id
+        }).sort({ date: -1 }); // Gets the most recent contacts first
+
+        res.json(contacts);
+    } catch (error) {
+        console.error(error.message);
+        res.status(500).send('Server Error');
+    }
 });
 
 // @route POST api/contacts
 // @desc Add new contact
 // @access Private
-router.post('/', () => {
-    res.send('Add a Contact');
+router.post('/', [
+    auth,
+    check('name', 'Name is required').not().isEmpty()
+], () => {
+
 });
 
 // @route POST api/contacts/:id
@@ -22,7 +39,7 @@ router.put('/:id', () => {
     res.send('Update Contact');
 });
 
-// @route POST api/contacts/:id 
+// @route POST api/contacts/:id
 // @desc Add new contact
 // @access Private
 router.delete('/:id', () => {
