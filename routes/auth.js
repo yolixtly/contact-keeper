d199d58eeab27c5cdb65d8aeb42e8395ad2c69ed
yolixtly <yolixtly_13@hotmail.com>
2020-01-13 18:22:25 -0800

    remove logs from routes

--- routes/auth.js
+++ routes/auth.js
@@ -18,7 +18,6 @@ router.post('/',
     ],
     async (req, res) => {
         const errors = validationResult(req);
-        console.log(errors);
         if (!errors.isEmpty()) {
             return res.status(400).json({
                 errors: errors.array()
--- routes/users.js
+++ routes/users.js
@@ -17,7 +17,7 @@ router.post('/',
     ],
     async (req, res) => {
         const errors = validationResult(req);
-        console.log(errors);
+        
         if (!errors.isEmpty()) {
             return res.status(400).json({
                 errors: errors.array()
