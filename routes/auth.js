43a85cb2b4bcdefe7586cbdb4e68238f46d2b186
yolixtly <yolixtly_13@hotmail.com>
2020-01-13 17:54:21 -0800

    Route for /api/auth authentication and get token

--- routes/auth.js
+++ routes/auth.js
@@ -1,18 +1,75 @@
 const express = require('express');
 const router = express.Router();
+const { check, validationResult } = require('express-validator');
+const User = require('../models/User');
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
+const config = require('config');
 
-// @route GET api/auth
-// @desc Get logged in user
-// @access Private
-router.get('/', () => {
-    res.send('Get logged in User');
-});
 
 // @route GET api/auth
 // @desc Auth user and get Token
 // @access Public
+router.post('/',
+    [
+        check('email', 'Please include a valid email').isEmail(),
+        check('password', 'Password is required').exists()
+    ],
+    async (req, res) => {
+        const errors = validationResult(req);
+        console.log(errors);
+        if (!errors.isEmpty()) {
+            return res.status(400).json({
+                errors: errors.array()
+            });
+        }
+
+        const { email, password } = req.body;
+
+        try {
+            let user = await User.findOne({ email });
+
+            // Did not find the user by email
+            if (!user) {
+                return res.status(400).json({ msg: 'Invalid Credentials' });
+            }
+
+            // Compare password
+            const isMatch = await bcrypt.compare(password, user.password);
+
+            if (!isMatch) {
+                return res.status(400).json({
+                    msg: 'Invalid Credentials'
+                })
+            }
+
+            const payload = {
+                user: {
+                    id: user.id
+                }
+            };
+
+            jwt.sign(payload, config.get('jwtSecret'), {
+                // expriresIn: 3600 // Production
+                expiresIn: 360000
+            }, (err, token) => {
+                if (err) {
+                    throw err;
+                }
+                res.json({ token })
+            });
+
+        } catch (error) {
+            console.error(error.message);
+            res.status(500).send('Server Error');
+        }
+    });
+
+// @route GET api/auth
+// @desc Get logged in user
+// @access Private
 router.post('/', () => {
-    res.send('Log in User');
+    res.send('Get logged in User');
 });
 
 module.exports = router;
