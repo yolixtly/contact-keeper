3fc045fcf0fd3aa79fbd05b930f661e59c0c81dc
yolixtly <yolixtly_13@hotmail.com>
2020-01-14 20:40:20 -0800

    Home includes contacts component

---
+++ client/src/components/contacts/Contacts.jsx
@@ -0,0 +1,19 @@
+import React, { Fragment, useContext } from 'react';
+import ContactContext from '../../context/contact/ContactContext';
+import ContactItem from './ContactItem';
+
+const Contacts = () => {
+    const contactContext = useContext(ContactContext);
+
+    const { contacts } = contactContext;
+
+    return (
+        <Fragment>
+            {contacts.map(contact => {
+                return <ContactItem contact={contact} key={contact.id} />;
+            })}
+        </Fragment>
+    );
+};
+
+export default Contacts;
