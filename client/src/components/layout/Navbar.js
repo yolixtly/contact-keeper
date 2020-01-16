d76f028c3ef8c0e4b75542d950dec90209177cae
yolixtly <yolixtly_13@hotmail.com>
2020-01-16 15:33:33 -0800

    Update Navbar with Auth Links and Guest Links

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -1,8 +1,40 @@
-import React from 'react';
+import React, { Fragment, useContext } from 'react';
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
+import AuthContext from '../../context/auth/AuthContext';
 
 const Navbar = ({ title, icon }) => {
+    const authContext = useContext(AuthContext);
+    const { isAuthenticated, logout, user } = authContext;
+
+    const onLogout = () => {
+        logout();
+        // Clear Contacts
+    }
+    const authLinks = (
+        <Fragment>
+            <li>Hello {user && user.name}</li>
+            <li>
+                <a href="#!">
+                    <i className="fas fa-sign-out-alt"></i> <span className="hide-sm" onClick={onLogout}>Logout</span>
+                </a>
+            </li>
+        </Fragment>
+    );
+
+    const guestLinks = (
+        <Fragment>
+            <li>
+                <Link to="/about">About</Link>
+            </li >
+            <li>
+                <Link to="/register">Register</Link>
+            </li>
+            <li>
+                <Link to="/login">Login</Link>
+            </li>
+        </Fragment>
+    );
     return (
         <div className="navbar bg-primary">
             <h1>
@@ -10,18 +42,7 @@ const Navbar = ({ title, icon }) => {
                 {title}
             </h1>
             <ul>
-                <li>
-                    <Link to='/'>Home</Link>
-                </li>
-                <li>
-                    <Link to="/about">About</Link>
-                </li>
-                <li>
-                    <Link to="/register">Register</Link>
-                </li>
-                <li>
-                    <Link to="/login">Login</Link>
-                </li>
+                {isAuthenticated ? authLinks : guestLinks}
             </ul>
         </div>
     )
