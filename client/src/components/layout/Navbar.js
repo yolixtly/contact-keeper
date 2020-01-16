601eca057be3ffb657af65c0560ab1242ace2d32
yolixtly <yolixtly_13@hotmail.com>
2020-01-15 17:13:10 -0800

    Login and Register Component

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
+
 const Navbar = ({ title, icon }) => {
     return (
         <div className="navbar bg-primary">
@@ -14,6 +15,12 @@ const Navbar = ({ title, icon }) => {
                 </li>
                 <li>
                     <Link to="/about">About</Link>
+                </li>
+                <li>
+                    <Link to="/register">Register</Link>
+                </li>
+                <li>
+                    <Link to="/login">Login</Link>
                 </li>
             </ul>
         </div>
