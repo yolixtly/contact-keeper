2a587936e6cceee0e9e4bbd21c65db0c3322c850
yolixtly <yolixtly_13@hotmail.com>
2020-01-15 22:12:07 -0800

    Wireup User registration with backend API

--- client/src/context/auth/AuthState.js
+++ client/src/context/auth/AuthState.js
@@ -1,6 +1,8 @@
 import React, { useReducer } from 'react';
 import AuthContext from './AuthContext';
 import AuthReducer from './AuthReducer';
+import axios from 'axios';
+
 import {
     REGISTER_SUCCESS,
     REGISTER_FAIL,
@@ -26,14 +28,48 @@ const AuthState = (props) => {
     const [state, dispatch] = useReducer(AuthReducer, initialState);
 
     // Load User (Checks which User is logged and get user data)
+    const loadUser = () => {
+        console.log("loadUser");
+    };
 
     // Register User (Signs up the user and gets a token back)
+    const register = async (formData) => {
+        const config = {
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        };
 
+        try {
+            // With the proxy setup on package.json, no need to put the entire url
+            const res = await axios.post('/api/users', formData, config);
+            dispatch({
+                type: REGISTER_SUCCESS,
+                payload: res.data
+            });
+        } catch (error) {
+            dispatch({
+                type: REGISTER_FAIL,
+                payload: error.response.data.msg
+            })
+        }
+    }
     // Login User (Login user and get token)
+    const login = () => {
+        console.log("login");
+    };
 
     // Logout
+    const logout = () => {
+        console.log("logout");
+    };
 
     // Clear Errors
+    const clearErrors = () => {
+        dispatch({
+            type: CLEAR_ERRORS
+        });
+    };
 
     return (
         <AuthContext.Provider
@@ -42,7 +78,12 @@ const AuthState = (props) => {
                 token: state.token,
                 isAuthenticated: state.isAuthenticated,
                 loading: state.loading,
-                error: state.error
+                error: state.error,
+                register,
+                loadUser,
+                logout,
+                login,
+                clearErrors
             }}>
             {props.children}
         </AuthContext.Provider>
