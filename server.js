2d3c7c21da302c040f78d63a8d15a3d42130c4c5
yolixtly <yolixtly_13@hotmail.com>
2020-01-17 12:11:00 -0800

    prepare for deploy

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const connectDB = require('./config/db');
-
+const path = require('path');
 const app = express();
 
 // Connect DB
@@ -20,6 +20,17 @@ app.use('/api/users', require('./routes/users'));
 app.use('/api/auth', require('./routes/auth'));
 app.use('/api/contacts', require('./routes/contacts'));
 
+// Serve Static Assets in Production
+if (process.env.NODE_ENV === 'production') {
+    // Set static folder
+    app.use(express.static('client/build'));
+
+    // Catch anything that is not the defined Routes
+    app.get('*', (req, res) => {
+        res.sendFile(__dirname, 'client', 'build',
+            'index.html');
+    });
+}
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`))
