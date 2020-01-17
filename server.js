83c5a0b64151e220936c0752731439568fff2776
yolixtly <yolixtly_13@hotmail.com>
2020-01-17 14:43:39 -0800

    modified server.js

--- server.js
+++ server.js
@@ -1,30 +1,30 @@
 const express = require('express');
 const connectDB = require('./config/db');
 const path = require('path');
+
 const app = express();
 
-// Connect DB
+// Connect Database
 connectDB();
 
-// Init Middleware (accepts the incoming body Data)
+// Init Middleware
 app.use(express.json({ extended: false }));
 
-// Routes
+// Define Routes
 app.use('/api/users', require('./routes/users'));
 app.use('/api/auth', require('./routes/auth'));
 app.use('/api/contacts', require('./routes/contacts'));
 
-// Serve Static Assets in Production
+// Serve static assets in production
 if (process.env.NODE_ENV === 'production') {
     // Set static folder
     app.use(express.static('client/build'));
 
-    // Catch anything that is not the defined Routes
-    app.get('*', (req, res) => {
-        res.sendFile(__dirname, 'client', 'build',
-            'index.html');
-    });
+    app.get('*', (req, res) =>
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+    );
 }
+
 const PORT = process.env.PORT || 5000;
-const HOST = process.env.HOST || '0.0.0.0';
-app.listen(PORT, HOST, () => console.log(`Server started on port ${PORT}`))
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
